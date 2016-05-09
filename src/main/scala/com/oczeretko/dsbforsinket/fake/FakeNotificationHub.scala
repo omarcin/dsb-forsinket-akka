52b210151f642d51e3c835ac4bdceea5f93738a5
Marcin Oczeretko <marcin.oczeretko@gmail.com>
2016-05-09 20:35:56 +0200

    Increase connection timeout for fake notification hub

--- src/main/scala/com/oczeretko/dsbforsinket/fake/FakeNotificationHub.scala
+++ src/main/scala/com/oczeretko/dsbforsinket/fake/FakeNotificationHub.scala
@@ -1,10 +1,12 @@
 package com.oczeretko.dsbforsinket.fake
 
 import java.util
+import java.util.concurrent.TimeUnit
 
 import com.oczeretko.dsbforsinket.ImplicitConversions
 import com.oczeretko.dsbforsinket.fake.FakeNotificationHubService.FakeNotificationModel
 import com.windowsazure.messaging._
+import okhttp3.OkHttpClient
 import org.apache.http.concurrent.FutureCallback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -20,9 +22,13 @@ case class FakeNotificationHub(baseUrl: String)(implicit ec: ExecutionContext) e
     new Retrofit.Builder()
       .baseUrl(baseUrl)
       .addConverterFactory(GsonConverterFactory.create())
+      .client(new OkHttpClient.Builder()
+        .readTimeout(60, TimeUnit.SECONDS)
+        .connectTimeout(60, TimeUnit.SECONDS)
+        .build())
       .build()
 
-  lazy val service: FakeNotificationHubService = retrofit.create(classOf[FakeNotificationHubService]);
+  lazy val service: FakeNotificationHubService = retrofit.create(classOf[FakeNotificationHubService])
 
   override def sendNotificationAsync(notification: Notification, tagExpression: String, callback: FutureCallback[AnyRef]): Unit = {
     val body = notification.getBody
